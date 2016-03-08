7d2620274d8df56eb6986c2f2ea70fb92914e5a7
DavidSeven <david07seven@gmail.com>
2016-03-08 06:46:24 -0500

    new file:   controllers/init.php  	new file:   public/css/styles.css  	deleted:    public/fonts/Roboto-Black.ttf  	deleted:    public/fonts/Roboto-Thin.ttf  	deleted:    public/fonts/RobotoCondensed-Light.ttf  	new file:   public/images/c.jpg  	new file:   public/images/d.jpg  	new file:   public/images/r.jpg  	new file:   public/images/u.jpg  	modified:   public/index.php

---
+++ controllers/init.php
@@ -0,0 +1,20 @@
+<?php
+  function head ()
+  {
+    ?>
+    <meta charset = "utf-8">
+    <title>Trabajos de grado</title>
+    <meta name = "viewport" content = "width = device-width, user-scalable = no, initial-sacel = 1">
+    <link rel = "stylesheet" href = "css/bootstrap.min.css">
+    <link rel = "stylesheet" href = "css/styles.css">
+    <?php
+  }
+
+  function javascript ()
+  {
+    ?>
+    <script src = "js/jquery.min.js"></script>
+    <script src = "js/bootstrap.min.js"></script>
+    <?php
+  }
+?>
--- public/index.php
+++ public/index.php
@@ -1,10 +1,54 @@
+<?php
+  require ('../controllers/init.php');
+?>
+
 <!DOCTYPE HTML>
 <html>
   <head>
+    <?php
+      head ();
+    ?>
   </head>
   <body>
+    <header>
+      <div class = "container">
+        <h1>Trabajos de grado</h1>
+      </div>
+    </header>
+    <div class = "container">
+      <section class = "main row-fluid">
+        <section class = "col-xs-12 col-sm-6 col-md-6 col-lg-3">
+          <a href = "#">
+            <div class = "circle container-fluid">
+              <img src = "images/c.jpg" alt = "Crear">
+            </div>
+          </a>
+        </section>
+        <section class = "col-xs-12 col-sm-6 col-md-6 col-lg-3">
+          <a href = "#">
+            <div class = "circle container-fluid">
+              <img src = "images/r.jpg" alt = "Leer">
+            </div>
+          </a>
+        </section>
+        <section class = "col-xs-12 col-sm-6 col-md-6 col-lg-3">
+          <a href = "#">
+            <div class = "circle container-fluid">
+              <img src = "images/u.jpg" alt = "Actualizar">
+            </div>
+          </a>
+        </section>
+        <section class = "col-xs-12 col-sm-6 col-md-6 col-lg-3">
+          <a href = "#">
+            <div class = "circle container-fluid">
+              <img src = "images/d.jpg" alt = "Eliminar" text-align = "center">
+            </div>
+          </a>
+        </section>
+      </section>
+    </div>
+    <?php
+      javascript ();
+    ?>
   </body>
 </html>
-<?php
-  echo 'Hi people, ¿How are you?';
-?>
