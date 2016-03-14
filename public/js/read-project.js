1c2e56dd6409431555cb7dec151427c18e0ee7a6
DavidSeven <david07seven@gmail.com>
2016-03-13 22:32:36 -0500

    modified:   public/js/read-project.js

--- public/js/read-project.js
+++ public/js/read-project.js
@@ -75,6 +75,7 @@ $(document).ready
               console.log ("Dataset:");
               console.log (data);
               var newRow = "";
+              var newFilterButtons ="<tr id = 'open-filters'><td colspan = '7'><div class = 'col-xs-12 col-sm-12 col-md-12 col-lg-12' id = 'button-main-div'><div class = 'container button-container' id = 'button-container-2'><div class = 'main row'><div class = 'col-xs-6 col-sm-6 col-md-6 col-lg-6'><input class = 'btn btn-lg btn-success button-width center-block' type = 'button' value = 'Filter' name = 'filter'></div><div class = 'col-xs-6 col-sm-6 col-md-6 col-lg-6'><input class = 'btn btn-lg btn-danger button-width center-block' type = 'button' value = 'Back' name = 'back-to-index' onclick = 'document.location=" + '"../public/index.php"' + "'/></div></div></div></div></td></tr>";
 
               for (var i = 0; i < size; i ++)
               {
@@ -90,7 +91,7 @@ $(document).ready
               }
 
               $("#table-read").append (newRow);
-              $("#table-read").append ($("#open-filters"));
+              $("#table-read").append (newFilterButtons);
             },
             error: function (data)
             {
